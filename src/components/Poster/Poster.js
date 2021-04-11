dff4cf39cdad544c93f980c0e67f1085e38185b5
Louis Grasset <louisgrasset@protonmail.com>
2021-04-11 08:46:33 +0200

    fix: remove class

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -2,7 +2,7 @@ import './Poster.scss';
 
 export const Poster = ({ url, togglePlay }) => {
     return (
-        <div className={'poster' + ' poster--animate'} onClick={togglePlay}>
+        <div className={'poster'} onClick={togglePlay}>
             <div className={'poster__background'} style={{ backgroundImage: `url(${url})` }}></div>
         </div>
     );
